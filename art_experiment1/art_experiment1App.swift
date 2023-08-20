74205e9da7688a9e676ec39e31fc6251e3eb41dc
Oliver Libaw <oliver_libaw@yahoo.com>
2023-08-20 11:51:58 -0700

    Initial Commit

---
+++ art_experiment1/art_experiment1App.swift
@@ -0,0 +1,17 @@
+//
+//  art_experiment1App.swift
+//  art_experiment1
+//
+//  Created by Oliver on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct art_experiment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
