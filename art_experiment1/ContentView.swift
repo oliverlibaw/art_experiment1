c89d29675c4e8bdd24089f71e77d5e93ed21dcca
oliverlibaw <92615450+oliverlibaw@users.noreply.github.com>
2023-08-21 10:12:48 -0700

    Update ContentView.swift

--- art_experiment1/ContentView.swift
+++ art_experiment1/ContentView.swift
@@ -1,26 +1,53 @@
-//
-//  ContentView.swift
-//  art_experiment1
-//
-//  Created by Oliver on 8/20/23.
-//
-
 import SwiftUI
 
+//  Testing GitHub
+
+
 struct ContentView: View {
+    @State var textBlocks = [String]()
+    @State var currentIndex = 0
+    let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect() // Timer every 2 minutes
+    
+    func loadTextBlocks() {
+        // Load text blocks file
+        let path = Bundle.main.path(forResource: "textBlocks", ofType: "txt")
+        if let textURL = path {
+            do {
+                let text = try String(contentsOf: URL(fileURLWithPath: textURL))
+                textBlocks = text.components(separatedBy: "\n\n")
+            } catch {
+                print("Error loading text blocks: \(error)")
+            }
+        }
+        // Set initial index
+        currentIndex = 0
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 20) {
+            // Display current text block if currentIndex is valid
+            if currentIndex < textBlocks.count {
+                let lines = textBlocks[currentIndex].components(separatedBy: "\n")
+                VStack(alignment: .leading, spacing: 10) {
+                    ForEach(lines.indices, id: \.self) { index in
+                        Spacer().frame(height: index == 0 ? 0 : 10)
+                        Text(lines[index])
+                            .font(index == 0 ? .largeTitle.bold() : .body)
+                            .padding(.leading)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            loadTextBlocks()
+        }
+        .onReceive(timer) { _ in
+            currentIndex += 1
+            if currentIndex == textBlocks.count {
+                currentIndex = 0
+            }
         }
         .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
